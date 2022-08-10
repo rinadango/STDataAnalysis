375fb782795c3a26a8fa8f4bdd93a52449d9328c
rindango <ipaulachan@gmail.com>
2022-08-10 14:05:45 +0300

    Added notes

--- FilterBYName.py
+++ FilterBYName.py
@@ -10,11 +10,12 @@ def filter_by_type_gene(gene_starts, variable_names: list):
 
     type_of_gene = type(gene_starts) # check type of variable
 
-    if type_of_gene == str:
+    if type_of_gene == str: # if just one indentifier
         for gene_id in variable_names:
             if gene_id.startswith(gene_starts):
                 print(gene_id)
-    elif type_of_gene == list:
+
+    elif type_of_gene == list: # if several
         for item in gene_starts:
             for gene_id in variable_names:
                 if gene_id.startswith(item):
