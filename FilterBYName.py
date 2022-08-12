722edbcb402e076ff0eeda5f67b0873ea601fa06
rindango <ipaulachan@gmail.com>
2022-08-12 12:07:07 +0300

    Changed some code lines

--- FilterBYName.py
+++ FilterBYName.py
@@ -3,20 +3,22 @@
 @author: Paulina Frolovaite
 
 `gene_starts` - identifier with what the gene name starts, for eg.: MT- for mitochondrials genes; INPUT: a string or a list of strings
-`variable_names` - a list of gene names/ids; INPUT: list of strings
+`anndata_object` - the imported data set in an anndata object having features
 """
 
-def filter_by_type_gene(gene_starts, variable_names: list):
+def filter_by_type_gene(gene_starts, anndata_object):
+
+    anndata_object = [names for names in anndata_object.var_names]
 
     type_of_gene = type(gene_starts) # check type of variable
 
     if type_of_gene == str: # if just one indentifier
-        for gene_id in variable_names:
+        for gene_id in anndata_object:
             if gene_id.startswith(gene_starts):
                 print(gene_id)
 
     elif type_of_gene == list: # if several
         for item in gene_starts:
-            for gene_id in variable_names:
+            for gene_id in anndata_object:
                 if gene_id.startswith(item):
                     print(gene_id)
